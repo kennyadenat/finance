0d9e71f5471d0844d9c60dedcf8a3d90c154463c
Kehinde Adelugba <kennyadenat09@gmail.com>
2020-08-31 22:24:00 +0100

    updated flutter packages

---
+++ lib/view/covid_landing.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CovidLanding extends StatefulWidget {
+  CovidLanding({Key key}) : super(key: key);
+
+  @override
+  _CovidLandingState createState() => _CovidLandingState();
+}
+
+class _CovidLandingState extends State<CovidLanding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(),
+    );
+  }
+}
